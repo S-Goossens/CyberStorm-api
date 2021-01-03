cbec97c6f8ddffb65c1b834b83efb05df092a8c6
Sidney Goossens <45096509+S-Goossens@users.noreply.github.com>
2021-01-03 10:34:24 +0100

    some work on authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
-const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const error = new Error('Validation failed.');
+        const error = new Error("Validation failed.");
         error.statusCode = 422;
         error.data = errors.array();
         throw error;
@@ -29,7 +29,7 @@ exports.signup = (req, res, next) => {
         })
         .then((result) => {
             res.status(201).json({
-                message: 'User created!',
+                message: "User created!",
                 userId: result._id,
             });
         })
@@ -50,7 +50,7 @@ exports.login = (req, res, next) => {
         .then((user) => {
             if (!user) {
                 const error = new Error(
-                    'A user with this email could not be found.'
+                    "A user with this email could not be found."
                 );
                 error.statusCode = 401;
                 throw error;
@@ -60,7 +60,7 @@ exports.login = (req, res, next) => {
         })
         .then((isEqual) => {
             if (!isEqual) {
-                const error = new Error('Wrong password.');
+                const error = new Error("Wrong password.");
                 error.statusCode = 401;
                 throw error;
             }
@@ -70,11 +70,13 @@ exports.login = (req, res, next) => {
                     userId: loadedUser._id.toString(),
                 },
                 process.env.TOKEN_PRIV,
-                { expiresIn: '1h' }
+                { expiresIn: "1h" }
             );
+            //todo: return expirationtime as well
             res.status(200).json({
                 token: token,
                 userId: loadedUser._id.toString(),
+                email: loadedUser.email,
             });
         })
         .catch((err) => {
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 // multer for saving of images
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-require('dotenv').config();
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
 
 // require routes
-const productRoutes = require('./routes/product');
-const authRoutes = require('./routes/auth');
+const productRoutes = require("./routes/product");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 //configure multer
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'images');
+        cb(null, "images");
     },
     filename: (req, file, cb) => {
         cb(null, uuidv4() + path.extname(file.originalname));
@@ -26,9 +26,9 @@ const fileStorage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
     if (
-        file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg'
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
     ) {
         cb(null, true);
     } else {
@@ -38,26 +38,30 @@ const fileFilter = (req, file, cb) => {
 
 app.use(bodyParser.json());
 app.use(
-    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        'Acces-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE'
+        "Acces-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
     );
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Requested-With"
     );
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
     next();
 });
 
 //app.use routes
-app.use('/products', productRoutes);
-app.use('/auth', authRoutes);
+app.use("/products", productRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
